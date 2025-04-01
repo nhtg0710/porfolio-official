a2f28d724a46d1604a1f43cb3636a49171d7a8c0
nhtg0710 <truonggiangh@student.unimelb.edu.au>
2025-04-01 16:41:02 +1100

    Change file path

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
           <div className="flex items-center gap-3">
             <figure className="img-box w-9 h-9 rounded-lg">
               <img
-                src="/images/avatar-1.jpg"
+                src="/images/avatar-1.jpeg"
                 width={40}
                 height={40}
                 alt="Zac Hoang"
@@ -42,7 +42,7 @@ const Hero = () => {
         <div className="hidden lg:block">
           <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
             <img
-              src="/images/headshot.jpg"
+              src="/images/headshot.jpeg"
               width={656}
               height={800}
               alt="Zac Hoang"
